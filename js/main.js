ba67eb73c569b2f44a195422fe73552abbe270a1
David W. McKelvey <david@mckelveycreative.com>
2013-02-24 18:09:37 -0800

    adding scroll js; basic basic content

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
 
+$(document).ready(function(){
+  
+  $('a[href^="#"]').click(function(e){
+    e.preventDefault();
+    var anchorName = $(this).attr('href'),
+        anchor = $(anchorName);
+    if (anchor.length > 0) {
+      $(document.body).animate({
+        scrollTop: $(anchorName).offset().top
+      }, 750, 'easeInOutQuint', function(){
+        window.location.hash = anchorName;
+      });
+    }
+  });
+  
+});
+
